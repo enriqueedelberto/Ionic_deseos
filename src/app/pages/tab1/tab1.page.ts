5af093ff514e8f53cdb8a2b89dcd119039dab46c
enriqueedelberto <enriqueviiireyes@gmail.com>
2019-06-23 17:08:34 -0500

    Alert controller

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,10 +11,40 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   constructor(public deseosService: DeseosService,
-              private router: Router) {
+              private router: Router,
+              private alertCtrl: AlertController) {
   }
 
-  agregarLista() {
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async agregarLista() {
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this.alertCtrl.create({
+         header: 'Nueva lista',
+         inputs: [
+           {
+             name: 'titulo',
+             type: 'text',
+             placeholder: 'Nombre de la lista'}
+          ],
+         buttons: [
+           {
+             text: 'Cancelar',
+             role: 'cancel',
+             handler: () => {
+               console.log('Cancelar');
+             }
+            },
+            {
+              text: 'Crear',
+              handler: (data) => {
+                if (data.titulo.length === 0) {
+                  return;
+                }
+                console.log(data);
+                this.deseosService.crearLista(data.titulo);
+              }
+            }]
+      });
+
+      await alert.present();
   }
 }
