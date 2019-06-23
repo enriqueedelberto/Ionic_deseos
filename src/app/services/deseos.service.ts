8336210e9957302628772b90e808346265c1b1a2
enriqueedelberto <enriqueviiireyes@gmail.com>
2019-06-23 17:26:32 -0500

    Local storage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,16 +9,28 @@ export class DeseosService {
   listas: Lista [] = [];
 
   constructor() {
-    const lista1 = new Lista('Recolectar infinity stones');
-    const lista2 = new Lista('Heroes');
 
-    this.listas.push (lista1, lista2);
-
+    this.cargarStorage ();
+    
    }
 
    crearLista( titulo: string) {
     const nuevaLista = new Lista(titulo); 
     this.listas.push( nuevaLista );
+    this.guardarStorage();
    }
+
+   guardarStorage() {
+
+    localStorage.setItem( 'data', JSON.stringify(this.listas) );
+   }
+   cargarStorage () {
+     if(  localStorage.getItem('data') ){
+      this.listas = JSON.parse( localStorage.getItem('data') );
+      return;
+     }
+   this.listas = [];
+  
+  }
 
 }
