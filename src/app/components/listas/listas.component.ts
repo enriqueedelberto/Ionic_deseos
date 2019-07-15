e36aa142bf9eb6b070034af64f42df8a4d988166
enriqueedelberto <enriqueviiireyes@gmail.com>
2019-07-14 22:24:29 -0500

    Edit list title

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista-model';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -9,11 +10,12 @@ import { Lista } from 'src/app/models/lista-model';
   styleUrls: ['./listas.component.scss'],
 })
 export class ListasComponent implements OnInit {
-
+  @ViewChild( IonList ) lista: IonList;
   @Input() terminada = true;
 
   constructor(public deseosService: DeseosService,
-             private router: Router) { }
+              private router: Router,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
@@ -30,6 +32,43 @@ this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`);
    borrarLista( lista: Lista ){
 
      this.deseosService.borrarLista( lista );
+   }
+
+   async editarNombreLista( lista: Lista ) {
+    const alert = await this.alertCtrl.create({
+      header: 'Editar lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          value: lista.titulo,
+          placeholder: 'Nombre de la lista'}
+       ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+            this.lista.closeSlidingItems();
+          }
+         },
+         {
+           text: 'Actualizar',
+           handler: (data) => {
+             if (data.titulo.length === 0) {
+               return;
+             }
+             
+             lista.titulo = data.titulo;
+             this.deseosService.guardarStorage();
+             this.lista.closeSlidingItems();
+           }
+         }]
+   });
+
+   // tslint:disable-next-line:align
+   await alert.present();
    }
 
 }
